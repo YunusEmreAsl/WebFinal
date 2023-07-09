49f6b5b4353dc834995945124d9e7fd8e698ffab
YunusEmreAsl <yunusemreasl.2310@gmail.com>
2023-07-09 17:35:42 +0300

    Final Ödevi

---
+++ WebFinal/src/test/java/com/yunus/webfinal/WebFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.webfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
